ee856495b696a800dc69a58369d42bf72371b560
RuanMorais-22 <s7temoney@gmail.com>
2025-09-18 17:27:07 -0300

    ci: add github actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/clientes_lara_v2/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
